a50a10e61f07a9b2a6385df1610f1a70253b6109
hamza <hmza312@users.noreply.github.com>
2021-01-26 15:20:46 +0500

    'pushleopard'

---
+++ service_worker.js
@@ -0,0 +1,10 @@
+function registerServiceWorker() {
+  return navigator.serviceWorker.register('/service-worker.js')
+  .then(function(registration) {
+    console.log('Service worker successfully registered.');
+    return registration;
+  })
+  .catch(function(err) {
+    console.error('Unable to register service worker.', err);
+  });
+}
